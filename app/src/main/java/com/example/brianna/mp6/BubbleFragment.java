dbc672b1d9f929c4c319b0f538486ad6d823a305
Rahzi <43010238+Rahzi@users.noreply.github.com>
2018-12-12 02:01:56 -0600

    Update BubbleFragment.java

--- app/src/main/java/com/example/brianna/mp6/BubbleFragment.java
+++ app/src/main/java/com/example/brianna/mp6/BubbleFragment.java
@@ -73,15 +73,6 @@ public class BubbleFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment BubbleFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static BubbleFragment newInstance(String param1, String param2) {
         BubbleFragment fragment = new BubbleFragment();
         Bundle args = new Bundle();
@@ -97,8 +88,6 @@ public class BubbleFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_bubble, container, false);
 
         return view;
@@ -186,17 +175,6 @@ public class BubbleFragment extends Fragment {
         super.onDetach();
         mListener = null;
     }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
